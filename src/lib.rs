3525ccc5213e1a75ca37f14479ae963c76f3168b
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-26 02:08:43 +0500

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,42 @@
 pub use std::ops::Range;
 
+pub fn search(range: Range<f64>, ltz: f64, eps: f64, f: impl Fn(f64) -> f64) -> f64 {
+    assert!(ltz > 0.0);
+    assert!(eps > 0.0);
+
+    let twoltz = 2.0 * ltz;
+    let invtwoltz = 1.0 / twoltz;
+
+    let mut pts = vec![
+        (range.start, f(range.start)),
+        (range.end, f(range.end)),
+    ];
+    let mut minp = (
+        invtwoltz * (f(pts[0].0) - f(pts[1].0) + ltz * (pts[0].0 + pts[1].0)),
+        0.5 * (f(pts[0].0) + f(pts[1].0) + ltz * (pts[0].0 - pts[1].0)),
+    );
+    let mut delta = invtwoltz * (f(minp.0) - minp.1);
+
+    while twoltz * delta > eps {
+        let x1 = minp.0 - delta;
+        let x2 = minp.0 + delta;
+        let y12 = 0.5 * (f(minp.0) + minp.1);
+        pts.extend_from_slice(&[
+            (x1, y12), (x2, y12)
+        ]);
+        
+        let mut minidx = 0;
+        let mut minval = pts[minidx].1;
+        for (i, p) in pts.iter().enumerate() {
+            if p.1 < minval {
+                minidx = i;
+                minval = p.1;
+            }
+        }
+        minp = (pts[minidx].0, minval);
+        pts.swap_remove(minidx);
+        delta = invtwoltz * (f(minp.0) - minp.1);
+    }
+
+    minp.0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use om_pl::*;
 
 fn main() {
-    println!("Polyline search!");
+    // x^2+x*ln(2+x^2) = x*(x+ln(1+(1+x^2)))
+    let f = |x: f64| x * (x + (1.0 + x * x).ln_1p());
+    let range = -10.0..10.0;
+    let ltz = 30.0;
+    let eps = 1e-4;
+    let x = search(range, ltz, eps, f);
+    println!("x              : {}", x);
+    println!("x^2+x*ln(2+x^2): {}", f(x));
+    println!("");
+
+    let f = |x: f64| x.sin() + (1.0 + x * x).ln_1p();
+    let range = 2.0..14.0;
+    let ltz = 1.3;
+    let eps = 1e-4;
+    let x = search(range, ltz, eps, f);
+    println!("x               : {}", x);
+    println!("sin(x)+ln(2+x^2): {}", f(x));
+    println!("");
+
+    let f = |x: f64| x * (5.0 * x * x - x - 4.0);
+    let range = -7.0..7.0;
+    let ltz = 750.0;
+    let eps = 1e-4;
+    let x = search(range, ltz, eps, f);
+    println!("x          : {}", x);
+    println!("5*x-x^2-4*x: {}", f(x));
+    println!("");
 }
